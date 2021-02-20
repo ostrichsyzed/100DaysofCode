082a03f5a062ea2d67da7b7eb5ab7a60b47691ba
Jasmine Watson <68076232+ostrichsyzed@users.noreply.github.com>
2021-02-20 16:33:24 -0700

    Add files via upload

---
+++ Day01.py
@@ -0,0 +1,12 @@
+#1. Create a greeting for your program.
+print("Struggling to find an awesome Band Name?")
+#2. Ask the user for the city that they grew up in.
+print("First, what city did you grow up in?")
+city = input(" ")
+#3. Ask the user for the name of a pet.
+print("What is your pet's name?")
+pet = input(" ")
+#4. Combine the name of their city and pet and show them their band name.
+print("You are shall be... " + city + " " + pet + "!")
+print("Play that funky music!")
+#5. Make sure the input cursor shows on a new line, see the example at:
