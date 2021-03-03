1c525e5d2d90505f4a3c7140582237d3d803baab
Jasmine Watson <68076232+ostrichsyzed@users.noreply.github.com>
2021-03-02 23:21:03 -0700

    Create Day006.py

---
+++ Day006.py
@@ -0,0 +1,27 @@
+# Helping Reeborg leave the maze
+# https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
+
+def turn_right():
+    turn_left()
+    turn_left()
+    turn_left()
+    
+def hurdle():
+    turn_left()
+    move()
+    turn_right()
+    move()
+    turn_right()
+    move()
+    turn_left()
+
+while not at_goal():
+    if front_is_clear():
+        move()
+    elif right_is_clear():
+        turn_right()
+        move()
+    elif wall_in_front():
+        turn_left()
+    elif wall_on_right():
+        move()
